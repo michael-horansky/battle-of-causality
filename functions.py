80db2dfa5a0bbd16c8209d75af7b79dad252aad6
michael-horansky <michael.horansky@gmail.com>
2025-01-08 23:58:45 +0000

    Deleted the old paradigm, canonised the Board_square paradigm

--- functions.py
+++ functions.py
@@ -2,9 +2,17 @@
 import numpy as np
 
 # Automatic string trimming
-def uniform_str(x, l=3):
+def uniform_str(x, l=3, pad_c='0'):
     x_str = str(x)
-    return('0'*(l-len(x_str))+x_str)
+    return(pad_c*(l-len(x_str))+x_str)
+
+# Centered string of length w
+def st(a, w, f = ' '):
+    if len(str(a)) >= w:
+        return(str(a))
+    else:
+        diff = w - len(str(a))
+        return(f * int(np.floor(diff / 2.0)) + str(a) + f * int(np.ceil(diff / 2.0)))
 
 # This generates a list [element, element, element...]
 def repeated_list(length, element=''):
@@ -59,5 +67,90 @@ def decode_tank_controls(pos0, a0, direction, a1=-1):
     elif direction == 't' or direction == 'turn':
         pos1 = pos0
     return(pos1, a1)
-    
+
+def human_readable_azimuth(a):
+    if a == 0:
+        return('up')
+    if a == 1:
+        return('right')
+    if a == 2:
+        return('down')
+    if a == 3:
+        return('left')
+    return('jump')
+
+def encode_azimuth(human_readable_a):
+    if human_readable_a in ['up', 'u', '^', '0']:
+        return(0)
+    if human_readable_a in ['right', 'r', '>', '1']:
+        return(1)
+    if human_readable_a in ['down', 'd', 'v', '2']:
+        return(2)
+    if human_readable_a in ['left', 'l', '<', '3']:
+        return(3)
+    return(None)
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   NOTUNDERLINE = '\033[24m'
+   LIGHT = '\033[22m' # removes BOLD without resetting color
+   DISABLE = '\033[02m'
+   END = '\033[0m'
+
+   class bg:
+        BLACK = '\033[40m'
+        RED = '\033[41m'
+        GREEN = '\033[42m'
+        ORANGE = '\033[43m'
+        BLUE = '\033[44m'
+        PURPLE = '\033[45m'
+        CYAN = '\033[46m'
+        GREY = '\033[47m'
+        DEFAULT = '\033[49m'
+
+
+"""More color options
+reset = '\033[0m'
+bold = '\033[01m'
+disable = '\033[02m'
+underline = '\033[04m'
+reverse = '\033[07m'
+ strikethrough = '\033[09m'
+  invisible = '\033[08m'
+
+   class fg:
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+
+    class bg:
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
+"""
 
